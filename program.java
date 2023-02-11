43b3addc92aabb4bda76e9f1596fcfd4badd3387
Vladislav <vlad-pugovkin2@tandex.ru>
2023-02-11 14:47:39 +0300

    Little update

--- program.java
+++ program.java
@@ -8,9 +8,27 @@ public class program {
         int userNum = scan.nextInt();
         int result = triangularNumber(userNum);
         System.out.printf("Результат: %d", result);
+        System.out.println();
+        recTriangularNumber(userNum,1);
     }
     public static int triangularNumber(int n) {
         int result = (n*(n+1))/2;       
         return result;        
-    }    
+    }
+    public static int recTriangularNumber(int n,int k) {
+        int result;
+        String msg = "";
+    
+         if(k==n+1) return 1;
+         for (int i = 1; i < k+1; i++) {
+            if(i!=k) msg += i + " + ";
+            else msg += i;
+         }
+            result = (k*(k+1))/2;
+            msg += " = " + result;
+            System.out.println(msg);
+
+           return recTriangularNumber(n, k+1);
+    
+       }    
 }
